9190843994293e82e6d46a81d7a4cfb1b1c6ed25
user <user@ubuntu.localdomain>
2013-04-10 15:00:51 -0300

    portal numero de estagiarios

--- app/controllers/portal_controller.rb
+++ app/controllers/portal_controller.rb
@@ -1,6 +1,4 @@
 class PortalController < ApplicationController
-  
-
 
   def empresas
     puts "PARAMS #{params}"
@@ -9,40 +7,32 @@ class PortalController < ApplicationController
       empresa = Empresa.find_by_nome params[:filtro_1][:empresa]
       ano = params[:filtro_1][:ano]
 
+      meses = ["01#{ano}", "02#{ano}","03#{ano}","04#{ano}","05#{ano}","06#{ano}",
+             "07#{ano}","08#{ano}","09#{ano}","10#{ano}","11#{ano}","12#{ano}"]
+
       @vagas = Array.new
       @contratacoes = Array.new
 
-        @@meses = ["01#{ano}", "02#{ano}","03#{ano}","04#{ano}","05#{ano}","06#{ano}",
-             "07#{ano}","08#{ano}","09#{ano}","10#{ano}","11#{ano}","12#{ano}"]
-
-      @@meses.each_with_index do |mes, i|
+      meses.each_with_index do |mes, i|
         data = CalendarioMes.find_by_mmyyyy mes
         fato_portal_empresa_mensal = data.fato_portal_empresa_mensals.where("empresa_id = #{empresa.id}")
-        @vagas[i.next] = fato_portal_empresa_mensal[0].num_vagas.round(2)
-        @contratacoes[i.next] = fato_portal_empresa_mensal[0].num_contratacoes.round(2)
-
+        @vagas[i.next] = fato_portal_empresa_mensal[0].num_vagas
+        @contratacoes[i.next] = fato_portal_empresa_mensal[0].num_contratacoes
       end
     
     elsif !params[:filtro_2].nil?
+      empresa = Empresa.find_by_nome params[:filtro_2][:empresa]
       ano = params[:filtro_2][:ano]
-      mes = params[:filtro_2][:mes]
 
-      @departamentos = Array.new
-      @salarios = Array.new
-      @infra = Array.new
-      @treinamento = Array.new
-      @outros = Array.new
+      meses = ["01#{ano}", "02#{ano}","03#{ano}","04#{ano}","05#{ano}","06#{ano}",
+             "07#{ano}","08#{ano}","09#{ano}","10#{ano}","11#{ano}","12#{ano}"]
 
-      data = CalendarioMes.find_by_mmyyyy "#{mes}#{ano}"
+      @num_estagiarios = Array.new
 
-      data.fato_financeiro_despesas.each_with_index do |dado, i|
-        indice = i.next
-        @departamentos[indice] = dado.departamento.nome
-        @salarios[indice] = dado.salario_total.round(2)
-        @infra[indice] = dado.custo_infraestrutura.round(2)
-        @treinamento[indice] = dado.custo_treinamento.round(2)
-        @outros[indice] = dado.custo_outros.round(2)
-
+      meses.each_with_index do |mes, i|
+        data = CalendarioMes.find_by_mmyyyy mes
+        fato_portal_empresa_mensal = data.fato_portal_empresa_mensals.where("empresa_id = #{empresa.id}")
+        @num_estagiarios[i.next] = fato_portal_empresa_mensal[0].num_estagiarios
       end
 
     elsif !params[:filtro_3].nil?
